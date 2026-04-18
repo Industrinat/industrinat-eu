09ee87ad70077d6d296917005bc8a344b6a023bd
Daniel Olsson <daniel.olsson@industrinat.se>
2026-04-18 09:07:37 +0200

    chore: ta bort Google Tag Manager + GA4 + Google Ads tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,26 +38,6 @@ export default function RootLayout({
   return (
     <html lang="sv">
       <head>
-        <Script id="gtm-consent-default" strategy="beforeInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('consent', 'default', {
-      'analytics_storage': 'denied',
-      'ad_storage': 'denied',
-      'ad_user_data': 'denied',
-      'ad_personalization': 'denied',
-      'wait_for_update': 500
-    });
-  `}
-</Script>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-M2KLC5L');`}
-        </Script>
         <Script
           id="umami"
           defer
@@ -67,14 +47,6 @@ export default function RootLayout({
         />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-M2KLC5L"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <Header />
         <div className="pt-20">
           {children}
